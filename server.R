591263a8f28f1f4471738bbd4d044b091517304f
who loves beef ! <linjinquan14@163.com>
2016-10-15 15:00:02 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 server <- shinyServer(function(input, output){
   output$clplot <- renderPlot({
+    load("consume.RData")
     d <- dist(consume)
     plot(hclust(d, method = input$method), 
             hang = -1)
