8dfa626bf1e4613faa5a7b43208ae9f2cbe3518b
who loves beef ! <linjinquan14@163.com>
2016-10-16 09:39:57 +0800

    Create server.R

---
+++ server.R
@@ -0,0 +1,15 @@
+library(shiny)
+
+server <- shinyServer(function(input, output){
+  output$clplot <- renderPlot({
+    d <- dist(consume[, -1], method = input$method1)
+    hc <- hclust(d, method = input$method2)
+    if(input$lab == TRUE){
+      plot(hc, hang = -1,
+           labels = consume[, 1])
+    } else {
+    plot(hc, hang = -1)
+    }
+    rect.hclust(hc, k = input$num, border = "red")
+  })
+})
