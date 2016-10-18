5a07ccf1c5ea934b55a87405d79d4ecc0601081f
Jinquan Lin <linjinquan14@163.com>
2016-10-18 12:38:51 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 load("consume.RData")
 
 ui <- shinyUI(fluidPage(
-  titlePanel(h2("Consumption level")),
+  titlePanel(h3("Consumption Level Cluster Analysis")),
   sidebarLayout(
      sidebarPanel(
        selectInput(
