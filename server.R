3df4adcf5ca206fabad171b50d524ee9c6e2e7cc
Jinquan Lin <linjinquan14@163.com>
2016-10-18 12:25:45 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,17 +1,25 @@
 library(shiny)
-load("consume.Rdata")
+require(graphics) 
 
 server <- shinyServer(function(input, output){
-  output$clplot <- renderPlot({
-    d <- dist(consume[, -1], method = input$method1)
-    hc <- hclust(d, method = input$method2)
+  output$hclplot <- renderPlot({
+    if(input$clustermethod == "Hierarchical"){
+    d <- dist(consume[-1], method = input$samdismethod)
+    hc <- hclust(d, method = input$cladismethod)
     if(input$lab == TRUE){
       plot(hc, hang = -1,
            labels = consume[, 1])
     } else {
     plot(hc, hang = -1)
     }
-    rect.hclust(hc, k = input$num, 
-                border = rainbow(input$num))
+    rect.hclust(hc, k = input$hnum, border = rainbow(input$hnum))
+    } else {
+     km <- kmeans(consume[-1], centers = input$knum)
+     consume_1 <- consume[-1]
+     plot(consume_1[, c(input$Variable1, input$Variable2)], 
+          col = km$cluster, pch = km$cluster)
+     points(km$centers[, c(input$Variable1, input$Variable2)], 
+            col = 1:input$knum, pch = 8, cex = 3)
+    }   
   })
 })
